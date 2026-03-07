c9b4c10a37d2c3925099d6e6f8651f9a70d609e2
Shreyya407 <your_email_here>
2026-03-07 11:06:48 +0530

    Optimal second largest

---
+++ ARRAYS/Secondlargest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={1,2,3,4,4};
+    int n=5;
+
+    int largest = arr[0];
+    int slargest = -1;
+
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest)
+        slargest=largest;
+        largest=arr[i];
+
+    }
+    cout<<slargest;
+}
